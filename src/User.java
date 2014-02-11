7acdebbe271bf13ffd57bd432505e763991a9e07
Panida <cp.panida@gmailcom>
2014-02-11 13:58:09 +0700

    merge

--- src/User.java
+++ src/User.java
@@ -8,46 +8,48 @@
  *
  * @author LotK
  */
-
 import java.util.*;
 
 public class User {
+
     private int id;
     private String name;
     private final ArrayList<Integer> listGroupID;
-    
+
     public User(String _name, int _id) {
         name = _name;
         id = _id;
         listGroupID = new ArrayList<>();
     }
+
     public int getID() {
         return this.id;
     }
+
     public String getName() {
         return this.name;
     }
+
     public ArrayList<Integer> getGroupID() {
         return this.listGroupID;
     }
-    
+
     public void addGroup(int groupID) {
-        if(listGroupID.indexOf(Integer.valueOf(groupID)) == -1) {
+        if (listGroupID.indexOf(Integer.valueOf(groupID)) == -1) {
             listGroupID.add(Integer.valueOf(groupID));
-        }
-        else {
+        } else {
             System.out.println("This user(" + this.name + ") is already in this group(" + groupID + ")");
         }
     }
+
     public void leaveGroup(int groupID) {
-        if(listGroupID.indexOf(Integer.valueOf(groupID)) != -1) {
+        if (listGroupID.indexOf(Integer.valueOf(groupID)) != -1) {
             listGroupID.remove(listGroupID.indexOf(Integer.valueOf(groupID)));
-        }
-        else {
+        } else {
             System.out.println("This user(" + this.name + ") is not in this group(" + groupID + ")");
         }
     }
-    
+
     public static void main(String args[]) {
         User a = new User("Panida", 3);
         System.out.println(a.getID() + " " + a.getName());
@@ -57,15 +59,15 @@ public class User {
         a.addGroup(7);
         ArrayList<Integer> group = a.getGroupID();
         Iterator<Integer> it = group.iterator();
-        while(it.hasNext()) {
+        while (it.hasNext()) {
             System.out.println(it.next());
         }
-        
+
         a.leaveGroup(4);
         a.leaveGroup(5);
-        
+
         it = group.iterator();
-        while(it.hasNext()) {
+        while (it.hasNext()) {
             System.out.println(it.next());
         }
     }
